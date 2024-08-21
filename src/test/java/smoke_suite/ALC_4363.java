7d1db1ce927b67cf52bdd545778b173417010c8a
fleek540 <fleekautomationtest@gmail.com>
2024-08-21 18:11:34 +0530

    Update ALC_4363.java

--- src/test/java/smoke_suite/ALC_4363.java
+++ src/test/java/smoke_suite/ALC_4363.java
@@ -2,6 +2,7 @@ package smoke_suite;
 
 import java.io.IOException;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 import Utilities.BaseClass;
@@ -16,12 +17,12 @@ public class ALC_4363 extends BaseClass{
 	@Test(priority =0,enabled=true, description = "Approve_transactions_branch_exchange_history_Verify_In_BonusApproval")
 	@Description("Approve_transactions_branch_exchange_history_Verify_In_BonusApproval")	
 	public void  Approve_transactions_branch_exchange_history_Verify_In_BonusApproval() throws IOException, InterruptedException {
-		
+		try {
 		Data pn=new Data();
 		Branches br1 = new Branches(alcDriver);
 		Bonus bn1 =new Bonus(alcDriver);
 		
-		pn.runNewmanALC_4362_4363();
+		pn.runNewmanALC_4363();
 		
 		AlchemyLoginPage loginAlchmey=new AlchemyLoginPage(alcDriver);
 		loginAlchmey.alc_adminlogin("+17783844311","778778");
@@ -29,7 +30,11 @@ public class ALC_4363 extends BaseClass{
 		
 		bn1.bonusApproval(Data.bonusName);
 		br1.verifyBonusInBranchExcHistory(Data.branch1_Number);
+		((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
 		
+	}catch(Exception e) {
+		((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+	}
 
 }
 }
