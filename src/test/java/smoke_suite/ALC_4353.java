1024794f0d20f52cf7ccec0a9f2ac92f1d0c9e45
fleek540 <fleekautomationtest@gmail.com>
2024-08-21 18:13:00 +0530

    Update ALC_4353.java

--- src/test/java/smoke_suite/ALC_4353.java
+++ src/test/java/smoke_suite/ALC_4353.java
@@ -5,6 +5,8 @@ import alchemy_Pages.AlchemyLoginPage;
 import alchemy_Pages.Brands;
 import alchemy_Pages.Orders;
 import io.qameta.allure.Description;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,6 +14,7 @@ public class ALC_4353 extends BaseClass {
     @Test(priority = 0, description = "Login on alchemy with admin user")
     @Description("Alchmey Login")
     public void loginAlchmey() throws InterruptedException {
+    	try {
         System.out.println("Alchmey Login");
         AlchemyLoginPage loginAlchmey=new AlchemyLoginPage(alcDriver);
         loginAlchmey.alc_adminlogin("+17783844311","778778");
@@ -38,11 +41,17 @@ public class ALC_4353 extends BaseClass {
         o.enterAuthCode("778899");
         o.clickSubmitAuthCode();
         o.search_byName(bonusname);
+        ((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+	}catch(Exception e) {
+		((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+	}
 
     }
     @Test(priority = 1, description = "Go to 'Brand' Section and Create a new brand by clicking on the '+' icon")
     @Description("Create a new brand")
     public void createNewBrand() throws InterruptedException {
+    	try {
         System.out.println("New brand create");
         Brands brands=new Brands(alcDriver);
         brands.clickBranchTab();
@@ -52,16 +61,27 @@ public class ALC_4353 extends BaseClass {
         brands.clickCreateBrandBtnAfterFillDetails();
         brands.search_byName(randomBrandName);
         Assert.assertEquals(brands.verifyCreatedBrand(), randomBrandName);
+        ((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+	}catch(Exception e) {
+		((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+	}
     }
     @Test(priority = 2, description = "Bonus order should be updated with the new brand")
     @Description("Search for newly created brand and save")
     public void searchNewlyCreatedBrandInBonus() throws InterruptedException {
         System.out.println("Search an existing bonus order and click 'Edit settings'");
+        try {
         Orders orders = new Orders(alcDriver);
         orders.searchBonus(bonusname);
         orders.clickSpecificOrdersBonus();
         orders.searchCreatedBrandInBonusAndSave(randomBrandName);
         orders.searchBonus(bonusname);
         Assert.assertEquals(orders.verifyChnagesInBounsForBrandName(), randomBrandName);
+        ((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+	}catch(Exception e) {
+		((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+	}
     }
 }
