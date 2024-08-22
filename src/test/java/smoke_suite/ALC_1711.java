6649e15bddaaf37dff005ac710fe026ba7059e92
fleek540 <fleekautomationtest@gmail.com>
2024-08-22 16:31:38 +0530

    Update ALC_1711.java

--- src/test/java/smoke_suite/ALC_1711.java
+++ src/test/java/smoke_suite/ALC_1711.java
@@ -28,7 +28,7 @@ public class ALC_1711 extends BaseClass {
 	}
 	@Test(priority =1,enabled=true, description = "Verify KG Recycled in Branch | Bonus Exchange History In Branch And Processor Before Bonus Pay")
 	public void kgRecycledAndBonusInBranchExcHistoryBeforeVoid() throws InterruptedException {
-
+try{
 		AlchemyLoginPage loginAlchmey=new AlchemyLoginPage(alcDriver);
 		loginAlchmey.alc_adminlogin("+17783844311","778778");
 		Thread.sleep(3000);
@@ -38,11 +38,18 @@ public class ALC_1711 extends BaseClass {
 		b1.bonusExchangeHistoryBranch(Data.branch1_Id1711);
 		b1.bonusExchangeHistoryBranch(Data.branch2_Id1711);
 		b1.bonusProcessorExcHistoryVerification(Data.processor_Id1711);
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
+
+
 
 	}
 	@Test(priority =2,enabled=true, description = "Verify Kg By Country and Kg In Philippines Before Void")
 	public void dashBoardVerificationsBeforeVoid() throws InterruptedException {
-
+try{
 		Dashboard d1= new Dashboard(alcDriver);
 		Dashboard.checkKgByCountry();
 
@@ -51,92 +58,131 @@ public class ALC_1711 extends BaseClass {
 
 		System.out.println("Kg By Country before void: " + kgByCountryBeforeVoid);
 		System.out.println("Kg By Philippines before void: " + kgByPhilippinesBeforeVoid);
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 
 	@Test(priority =3,enabled=true, description = "Bonus Order's Exchange history | BUY/SELL transaction from Bonus order approval screen before Void")
 	public void bonusOrderExcHisVerificationBeforeVoid() throws InterruptedException {
-
+try{
 		Orders o1= new Orders(alcDriver);
 		o1.bonusExcHisVerification(Data.bonusOrderId1711);
 		o1.buySellVerification_1711beforeVoid(Data.bonusOrderId1711);
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	
 	//pbapp-1526
 	
 	@Test(priority =4,enabled=true, description = "Check member exchange history | Associated bonus from branch and Processor exchange history")
 	public void ExcHisBonusVerificationBeforeVoid() throws InterruptedException {
-
+try{
 		Members m1= new Members(alcDriver);
 		Branches b1= new Branches(alcDriver);
 		m1.memberExcHisBeforeVoid_1711();
 		b1.bonusBranchExcHistoryVerification(Data.branch1_Id1711);
 		b1.bonusBranchExcHistoryVerification(Data.branch2_Id1711);
 		b1.bonusProcessorExcHistoryVerification(Data.processor_Id1711);
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	//@Test(priority =5,enabled=true, description = "Verify Tokens in app for Member, Branch 1 & 2 before Void")
 	public void verifyTokenInAppBeforeBonusPaid() throws InterruptedException {
-
+try{
 		PB_Transaction p1 =new PB_Transaction(pbDriver);
 		p1.verifyBonusInAppMem_1711_BeforeBonusPaid();
 		p1.branchBonusPresent2(Data.branch1_Number1711,password,"30");
 		p1.branchBonusPresent2(Data.branch2_Number1711,password,"14");
-
+((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
 
 
 	}
 	@Test(priority =6,enabled=true, description = "Approve Bonus For member and Branches")
 	public void approveBonus() throws InterruptedException {
-
+try{
 		Bonus b1= new Bonus(alcDriver);
 		b1.payBonus1711();
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 
 
 	}
 	@Test(priority =7,enabled=true, description = " Member should get the correct bonus in Alchemy | Branch 1 & 2 should get the correct bonus in Alchemy " )
 	public void verifyBonusInAlchmeyAfterBonusPaid() throws InterruptedException {
-
+try{
 		Branches b2= new Branches(alcDriver);
 		Members m2= new Members(alcDriver);
 		m2.memberBonus(Data.member_Id1711,"133");
 		b2.verifyBonusInAlchmeyBranches_1711(Data.branch1_Id1711,"30");
 		b2.verifyBonusInAlchmeyBranches_1711(Data.branch2_Id1711,"14");
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	@Test(priority =8,enabled=true, description = " Member should get the correct bonus in App | Branch 1 & 2 should get the correct bonus in App")
 	public void verifyBonusInAppAfterBonusPaid() throws InterruptedException {
-
+try{
 		PB_Transaction p2 =new PB_Transaction(pbDriver);
 		p2.verifyBonusInAppMem_AfterBonusPaid(Data.member_Number1711.replace("+63",""),password);
 		p2.verifyBonusInAppB1_AfterBonusPaid(Data.branch1_Number1711.replace("+63",""),password);
 		p2.verifyBonusInAppB1_AfterBonusPaid(Data.branch2_Number1711.replace("+63",""),password);
-
+((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
 
 	}
 	@Test(priority =9,enabled=true, description = "Void Transaction")
 	public void voidTransaction() throws InterruptedException {
+		try{
 		Branches b1=new Branches(alcDriver);
 		b1.voidTransaction(Data.branch1_Id1711);
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	@Test(priority =10,enabled=true, description = "Verify Kg recycled in Branch 1 & 2 | Verify Associated bonus from branch 1 & 2 exchange history ")
 	public void kgRecycledInBranchAfterVoid() throws InterruptedException {
 
-
+try{
 		Branches b1= new Branches(alcDriver);
 		b1.kgRecycledInBranch(Data.branch1_Id1711,"0");
 		b1.kgRecycledInBranch(Data.branch2_Id1711,"0");
 		b1.bonusExchangeHistoryBranchAfterVoid(Data.branch1_Id1711);
 		b1.bonusExchangeHistoryBranchAfterVoid(Data.branch2_Id1711);
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 
 	}
 	@Test(priority =11,enabled=true, description = " Verify total KG and KG by Country for the specific country will be deducted based on the voided KG in Dashboard ie. 19 KG ")
 	public void dashBoardVerificationsAfterVoid() throws InterruptedException {
-
+try{
 		Dashboard d1= new Dashboard(alcDriver);
 
 		Dashboard.checkKgByCountry();
@@ -165,21 +211,34 @@ public class ALC_1711 extends BaseClass {
 		catch (Exception e){
 			System.out.println("Some Transaction happend while execution of the code");
 		}
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	@Test(priority =12,enabled=true, description = "In Bonus Order's Exchange history, transaction will be removed ")
 	public void bonusOrderExcHisVerificationAfterVoid() throws InterruptedException {
-
+try{
 		Orders o1= new Orders(alcDriver);
 		o1.bonusExcHisVerification(Data.bonusOrderId1711);
 		o1.buySellVerification_1711AfterVoid(Data.bonusOrderId1711);
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	@Test(priority =13,enabled=true, description = " KG from voided transaction is deducted in Bonus Progress under the Bonus Order Summary tab.")
 	public void bonusProgressInSummaryTabAfterVoid() throws InterruptedException {
-
+try{
 		Orders o1= new Orders(alcDriver);
 		o1.bonusProgressInSummaryTab("0 KG");
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 
 	@Test(priority =14,enabled=true, description = "Reclaim the Token")
@@ -187,42 +246,65 @@ public class ALC_1711 extends BaseClass {
 
 		Branches b1= new Branches(alcDriver);
 		b1.reclaimToken(Data.branch1_Id1711);
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	@Test(priority =15,enabled=true, description = " Verify Bonus will be reclaimed from APP member profile and In Branch 1 & 2 Profile ")
 	public void verifyBonusInAppAfterReclaimToken() throws InterruptedException {
-
+try{
 		PB_Transaction p2 =new PB_Transaction(pbDriver);
 		p2.verifyBonusInAppMem_1711_AfterVoid();
 		p2.verifyBonusInAppB1_AfterBonusPaid(Data.branch1_Number1711.replace("+63", ""),password);
 		p2.verifyBonusInAppB1_AfterBonusPaid(Data.branch2_Number1711.replace("+63", ""),password);
-
+((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
 
 	}
 	@Test(priority =16,enabled=true, description = "Verify Bonus will be reclaimed from Alchemy member profile and In Branch 1 & 2 Profile ")
 	public void verifyBonusInAlchmeyAfterReclaimToken() throws InterruptedException {
-
+try{
 		Branches b2= new Branches(alcDriver);
 		Members m2= new Members(alcDriver);
 		m2.memberBonus(Data.member_Id1711,"0");
 		b2.verifyBonusInAlchmeyBranches_1711(Data.branch1_Id1711,"0");
 		b2.verifyBonusInAlchmeyBranches_1711(Data.branch2_Id1711,"0");
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 
 	@Test(priority =17,enabled=true, description = "In Bonus Order's Exchange history, following transactions :  branch 1 -> branch 2 transaction and branch2  to Processor transactions is also automatically removed ")
 	public void bonusExchangeHistoryTransaction() throws InterruptedException {
-
+try{
 		Orders o1= new Orders(alcDriver);
 		o1.bonusExcHisVerification(Data.bonusOrderId1711);
-
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	@Test(priority =18,enabled=true, description = "Associated bonus from branch exchange history and processor exchange history will be removed for transactions")
 	public void branchExchangeHistoryBonus() throws InterruptedException {
+		try{
 		Branches b1= new Branches(alcDriver);
 		b1.bonusBranchExcHistoryAfterVoidVerification(Data.branch1_Id1711);
 		b1.bonusBranchExcHistoryAfterVoidVerification(Data.branch2_Id1711);
 		b1.bonusProcessorExcHistoryVerification(Data.processor_Id1711);
+((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 
 
