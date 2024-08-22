933ad9f4b207ba472d80b83ffc1e97a0de4d764c
fleek540 <fleekautomationtest@gmail.com>
2024-08-22 17:07:07 +0530

    Update ALC_4359.java

--- src/test/java/smoke_suite/ALC_4359.java
+++ src/test/java/smoke_suite/ALC_4359.java
@@ -26,33 +26,63 @@ public class ALC_4359 extends BaseClass {
 
     @Test(priority = 1, description = "Use and existing member and Uncheck bonus eligibility in the member profile")
     public void editMemberUncheckBonus() throws InterruptedException {
+        try{
         AlchemyLoginPage loginAlchmey=new AlchemyLoginPage(alcDriver);
         loginAlchmey.alc_adminlogin(adminphoneNumber,adminpassword);
         Members mb = new Members(alcDriver);
         mb.editMemberUncheckBonus(Data.member_Number4359);
+      ((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}      
     }
 
     @Test(priority = 3, description = "Notice that no bonus is generated in the entire transaction | Branch exchange history should show System Offset under Associated Bonus")
     public void verifyBonusPlasticBankWithBranch() throws InterruptedException, IOException {
+        try{
         PB_LoginPage pblogin = new PB_LoginPage(pbDriver);
         PB_Transaction pbTransaction = new PB_Transaction(pbDriver);
         pblogin.login(Data.branch1_Number4359.replace("+63", ""),password,"+63");
         pbTransaction.m1toB1(Data.member_Number4359);
         pbTransaction.checknobonusinhistory();
+      ((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
+        try{
         Branches b= new Branches(alcDriver);
         b.verifysystemoffsetbonus(Data.branch1_Number4359);
-        
+        ((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
        
     }
     @Test(priority = 4, description = "Processor exchange history should show System Offset under Associated Bonus")
     public void verifyBonusPlasticBankWithProcessor() throws InterruptedException {
+        try{
         PB_LoginPage pblogin = new PB_LoginPage(pbDriver);
         PB_Transaction pbTransaction = new PB_Transaction(pbDriver);
         pblogin.login(Data.processor_Number4359.replace("+63", ""),password,"+63");
         pbTransaction.b3ToPnb(Data.branch1_Number4359);
         pbTransaction.checknobonusinhistory();
+((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
+       try{     
         Processors p= new Processors(alcDriver);
         p.checkprocessorexchangehistorysysoff(Data.processor_Number4359);
-      
+      ((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
     }
 }
