782a3a70d5eda257e72d8e6e96160bfa14092a11
fleek540 <fleekautomationtest@gmail.com>
2024-08-22 16:34:55 +0530

    Update ALC_347.java

--- src/test/java/smoke_suite/ALC_347.java
+++ src/test/java/smoke_suite/ALC_347.java
@@ -16,32 +16,50 @@ public class ALC_347 extends BaseClass{
 	@Test(priority =1, description="Create a member on alchemy and give 1000 tokens into it's wallet")
 	@Description("Create a member on alchemy and give 1000 tokens into it's wallet")
 	public void create_a_member_with_tokens() throws InterruptedException {
-		
+		try{
 		AlchemyLoginPage loginAlchmey=new AlchemyLoginPage(alcDriver);
 		loginAlchmey.alc_adminlogin("+17783844311","778778");
 		Members m = new Members(alcDriver);
 		m.createRandomMemberWithTokens(Members.philippines, "+63", "1000");
+		((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
 		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 
 	@Test(priority=2, description="Login into app and verify the presence of tokens in the member wallet in app")
 	@Description("Login into app and verify the presence of tokens in the member wallet in app")
 	public void verify_tokens_present_in_app() throws InterruptedException {
+		try{
 		PB_LoginPage pl = new PB_LoginPage(pbDriver);
 		pl.login(Members.rnewnum,"123456a","+63");
 		Thread.sleep(5000);
 		PB_Member_Profile pmp = new PB_Member_Profile(pbDriver);
 		pmp.verifytokensarepresent();
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
 	}
 	@Test(priority=3, description="Reclaim 1000 tokens from the member on alchemy.")
 	@Description("Reclaim 1000 tokens from the member on alchemy.")
 	public void reclaim_tokens_on_alchemy() {
+		try{
 		Members m = new Members(alcDriver);
 		m.reclaimtokens();
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	@Test(priority=4, description="Verify the absence of tokens in member wallet in app.")
 	@Description("Verify the absence of tokens in member wallet in app.")
 	public void verify_tokens_not_present_in_app() throws InterruptedException {
+		try{
 		PB_LoginPage pl = new PB_LoginPage(pbDriver);
 		pl.logoutmem();
 		PB_LoginPage pl1 = new PB_LoginPage(pbDriver);
@@ -49,6 +67,12 @@ public class ALC_347 extends BaseClass{
 		Thread.sleep(5000);
 		PB_Member_Profile pmp = new PB_Member_Profile(pbDriver);
 		pmp.verifytokensarenotpresent();
+			 ((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
 	}
 	
 }
