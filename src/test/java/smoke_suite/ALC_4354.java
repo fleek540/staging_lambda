1802c8d95563c7129c131cfd09aff7273a865d53
fleek540 <fleekautomationtest@gmail.com>
2024-08-22 16:59:26 +0530

    Update ALC_4354.java

--- src/test/java/smoke_suite/ALC_4354.java
+++ src/test/java/smoke_suite/ALC_4354.java
@@ -26,6 +26,7 @@ public class ALC_4354 extends BaseClass {
 	@Test(priority =0, description = "New Processor account should be successfully created")
 	@Description("Create Processor with already Created Branch")
 	public void createProcessor() throws InterruptedException, IOException {
+		try{
 		Data d = new Data();
 		d.createMember4864();
 		System.out.println("Create Processor with already Created Branch");
@@ -33,22 +34,33 @@ public class ALC_4354 extends BaseClass {
 		l.login(Data.member_Number4864.replace("+63", ""),"123456a","+63");
 		PB_Register_Processor cbp =new PB_Register_Processor(pbDriver);
 		cbp.createprocessoraftermember();
+			 ((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
 	}
 	@Test(priority =1, description = "New processor account is searchable in Alchemy under the 'Processor' section | -Changes are saved and can be seen in user details")
 	@Description("Search and Edit Processor In Alchmey")
 	public void seacrchAndEditProcessorInAlchmey() throws InterruptedException {
-
+try{
 		System.out.println("Search and Edit Processor In Alchmey");
 		AlchemyLoginPage alp = new AlchemyLoginPage(alcDriver);
 		alp.alc_adminlogin(adminphoneNumber, adminpassword);
 		Processors apt = new Processors(alcDriver);
 		System.out.println(Data.member_Number4864);
 		apt.SearchAndEditProcessorInAlchemy(Data.member_Number4864);
+	((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}
 	}
 	@Test(priority =2, description = "User should see the updated data")
 	@Description("Login In app and Verify")
 	public void VerifyInApp() throws InterruptedException {
-
+try{
 		System.out.println("Login In app and Verify");
 		Thread.sleep(2000);
 		PB_LoginPage l=new PB_LoginPage(pbDriver);
@@ -58,25 +70,44 @@ public class ALC_4354 extends BaseClass {
 		tap(140,2060);
 		tap(60,155);
 		tap(330,1369);	
+	 ((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
 	}
 	
 	@Test(priority=3,description="Create processor from sign up page directly")
 	@Description("Create processor from sign up page directly")
 	public void createprocessorfromsignuppage() throws InterruptedException {
-		
+		try{
 		PB_Register_Processor cbp =new PB_Register_Processor(pbDriver);
 		cbp.createprocessordirectly();
+			 ((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}
 	}
 	
 	@Test(priority=4,description="Edit details in alchemy for this processor")
 	@Description("Edit details in alchemy for this processor")
 	public void edit() throws InterruptedException {
+		try{
 		Processors apt = new Processors(alcDriver);
 		apt.SearchAndEditProcessorInAlchemy("+63"+PB_Register_Processor.typedNumber);
+		((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "passed");
+		
+		}catch(Exception e) {
+			((JavascriptExecutor) alcDriver).executeScript("lambda-status=" + "failed");
+		}	
 	}
 	@Test(priority=5,description="Check edited details in app after logging in")
 	@Description("Check edited details in app after logging in")
 	public void check() throws InterruptedException {
+		try{
 		PB_LoginPage l=new PB_LoginPage(pbDriver);
 		l.login(PB_Register_Processor.typedNumber,"123456a","+63");
 		l.menu();
@@ -84,6 +115,12 @@ public class ALC_4354 extends BaseClass {
 		tap(140,2070);
 		tap(60,155);
 		tap(330,1369);	
+	 ((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"passed\", \"remark\":\"This is a passed test \"}} ");
+	        
+		}catch(Exception e) {
+			((JavascriptExecutor) pbDriver).executeScript("lambda-hook: {\"action\": \"setTestStatus\",\"arguments\": {\"status\":\"failed\", \"remark\":\"This is a failed test \"}} ");
+	        
+		}		
 	}
 
 }
